4eaf156925b852d7986ec7d84f41a86666633b10
Your Name <you@example.com>
2016-03-16 21:41:50 -0400

    Project TIme1

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssesmentProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Problem #1**************************************************************
+            // Console.WriteLine("Please Enter A Number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+            //if (number < 10)
+            //{
+            //    Console.WriteLine("This number is too small");
+
+            //}
+            //if (number >= 10)
+            //{
+            //    Console.WriteLine("This number is big enough");
+            //}
+
+
+
+            //Problem #2***************************************************************
+            //int[] numbers = { 9, 11, 7, 4, 3 };
+            //Console.WriteLine(numbers[1]);
+
+
+
+            //Problem #3****************************************************************
+            //int[] numbers = { 9, 11, 7, 4, 3 };
+
+            //foreach (int n in numbers)
+            //{
+
+            //    {
+            //        Console.Write(n);
+            //    }
+
+            //Problem #4****************************************************************
+            //Console.WriteLine("Please Enter A Number: ");
+            //int numberTime = int.Parse(Console.ReadLine());
+
+            //switch (numberTime)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Only one?");
+            //        break;
+            //    case 100:
+            //        Console.WriteLine("100? That's a lot!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Input not recognized");
+            //        break;
+            //}
+
+
+
+
+
+        }
+    }
+}
